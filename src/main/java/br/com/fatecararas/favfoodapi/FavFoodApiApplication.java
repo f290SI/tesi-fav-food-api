0040fd728a1cedb6ee2583b074f031da17c19d1b
Esdras Bezerra da Silva <esdrasilva@gmail.com>
2021-06-02 22:35:54 -0300

    Conclusão Fase 1.

--- src/main/java/br/com/fatecararas/favfoodapi/FavFoodApiApplication.java
+++ src/main/java/br/com/fatecararas/favfoodapi/FavFoodApiApplication.java
@@ -1,8 +1,13 @@
 package br.com.fatecararas.favfoodapi;
 
+import br.com.fatecararas.favfoodapi.domain.Ingrediente;
+import br.com.fatecararas.favfoodapi.domain.Receita;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FavFoodApiApplication implements CommandLineRunner {
@@ -13,6 +18,21 @@ public class FavFoodApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//TODO: Testar o relacionamnto dos objetos que representem a abstração de pelo menos 01 (uma) receita.
+		//TODO: Testar o relacionamento dos objetos que representem a abstração de pelo menos 01 (uma) receita.
+
+		var receitaPudimLeiteCondensado = new Receita("Pudim de Leite Condensado","https://www.goolgphotps.com/pdlc.png","Misturar todos os ingredientes...");
+
+		var ingrediente1 = new Ingrediente(8D,"Unidade","Gemas");
+		var ingrediente2 = new Ingrediente(1D,"Litro","Leite");
+		var ingrediente3 = new Ingrediente(1.5D,"Quilo","Leite Condensado");
+
+		List<Ingrediente> ingredientes = Arrays.asList(ingrediente1, ingrediente2, ingrediente3);
+
+		receitaPudimLeiteCondensado.setIngredientes(ingredientes);
+
+
+		System.out.println(receitaPudimLeiteCondensado);
+
+
 	}
 }
